cef6b91b89fb07908feba8cd7d4b4a29ea498ca6
Jannatul Maisha <60164071+JMaisha@users.noreply.github.com>
2021-03-27 17:34:07 -0400

    Add files via upload
    Fixed a bug that was starting the game on load

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomDiceimg1 = "images/dice" + randomNumber1 + ".png";
-document.querySelector(".img1").setAttribute("src", randomDiceimg1);
+if(performance.navigation.type == performance.navigation.TYPE_RELOAD){
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceimg1 = "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".img1").setAttribute("src", randomDiceimg1);
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDiceimg2 = "images/dice" + randomNumber2 + ".png";
-document.querySelector(".img2").setAttribute("src", randomDiceimg2);
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceimg2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelector(".img2").setAttribute("src", randomDiceimg2);
 
-if (randomNumber1 == randomNumber2) {
-  document.querySelector("h1").textContent = "Draw!";
-} else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").textContent = "Player 1 Wins! 🚩";
-} else {
-  document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+  if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").textContent = "Draw!";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins! 🚩";
+  } else {
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+  }
 }
